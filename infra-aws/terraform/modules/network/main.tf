79daf103d8389c3443a4ad00c80186aea7c9afc2
pdelaroche <pabloroche7@gmail.com>
2026-01-29 16:43:33 +0100

    network module: add full access via ssh conexion

--- infra-aws/terraform/modules/network/main.tf
+++ infra-aws/terraform/modules/network/main.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "monitoring" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["79.117.230.49/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
